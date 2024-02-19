b07c8ad0d30b5bbb5aadb4224498a1b9e0570e15
tom hikari <tomprescott1565@gmail.com>
2024-02-19 14:53:51 +0100

    mock cookie

--- test/cookie.test.ts
+++ test/cookie.test.ts
@@ -1,9 +1,30 @@
+import {
+  RequestCookie,
+  ResponseCookies,
+} from "next/dist/compiled/@edge-runtime/cookies";
 import Cookie from "../src/lib/cookie/cookies";
 import { cookies } from "next/headers";
 
+beforeAll(() => {
+  jest.mock("cookies");
+  cookies().set = jest.fn((name: string, value: string): ResponseCookies => {
+    localStorage.setItem(name, value);
+    return {} as ResponseCookies;
+  });
+
+  cookies().delete = jest.fn((name: string): ResponseCookies => {
+    localStorage.removeItem(name);
+    return {} as ResponseCookies;
+  });
+
+  cookies().get = jest.fn((name: string): RequestCookie => {
+    return { value: localStorage.getItem(name) } as RequestCookie;
+  });
+});
+
 afterAll(() => {
   const cookie = new Cookie(cookies, "sessionId");
-  cookie.remove();
+  return cookie.remove();
 });
 
 describe("Cookies test", () => {
