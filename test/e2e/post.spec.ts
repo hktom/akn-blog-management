7eafed09991fa425301f7e8b08fa52ccf0d78b54
tom hikari <tomprescott1565@gmail.com>
2024-02-22 14:13:38 +0100

    show post

---
+++ test/e2e/post.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect, Page } from "@playwright/test";
+import { fakePost } from "../unit/post/fakePost";
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/login");
+  await page.getByLabel("Email").fill("Sincere@april.biz");
+  await page.getByLabel("Password").fill("erTy3r4t");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.waitForURL("http://localhost:3000");
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test("render all post", async () => {
+  await expect(page.getByRole("heading", { name: "Posts" })).toBeVisible();
+});
+
+test("show post", async () => {
+  const row = await page.getByRole("row").nth(4);
+  const title = await row.getByRole("cell").textContent();
+
+  await row.click();
+  await page.waitForURL("http://localhost:3000/97");
+
+  await expect(
+    page.getByRole("heading", { name: title as string })
+  ).toBeVisible();
+
+  await expect(page.getByText("Author")).toBeVisible();
+  await expect(page.getByText("Comments")).toBeVisible();
+});
